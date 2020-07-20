14d47cc32acd6fd8dcd9ee75a3ccaafe651c9687
Yatin Kapur <yatin.aol@gmail.com>
2020-07-20 10:56:05 -0400

    push to repo

---
+++ create_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task` varchar(150) NOT NULL,
+  `finish_by` date NOT NULL,
+  `completed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+);
